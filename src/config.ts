3933ecbc5a12f8cafe0bc354906c6399c531914d
RealRusLink <real.rus.link@gmail.com>
2026-04-29 16:23:56 +0300

    refactored config loading

--- src/config.ts
+++ src/config.ts
@@ -3,53 +3,19 @@ import { generateKeyPairSync } from "node:crypto";
 
 export type hashVersions = "default";
 
-interface ConfigIF {
-    webPath: string;
-    listenPort: number;
-    db: {
-        host: string;
-        port: number;
-        name: string;
-        user: string;
-        password: string;
-        tables: {
-            users: string;
-            refreshTokens: string;
-        };
-    };
-    issuer: string;
-    crypto: {
-        currentVersion: hashVersions;
-        default: {
-            iterations: number;
-            keyLength: number;
-            digest: string;
-            saltLength: number;
-        };
-        jwtTTL: number;
-        refreshTTL: number;
-        refreshTimeout: number;
-        key: {
-            private: string;
-            public: string;
-            updated: number;
-        };
-    };
-}
+const loadData = () => {
+    dotenv.config({ quiet: true });
+    const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: { type: 'spki', format: 'pem' },
+        privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+    });
 
-export class Config implements ConfigIF {
-    public webPath: string;
-    public listenPort: number;
-    public db: ConfigIF["db"];
-    public issuer: string;
-    public crypto: ConfigIF["crypto"];
-
-    constructor() {
-        dotenv.config({ quiet: true });
-
-        this.webPath = process.env.WEB_PATH || "";
-        this.listenPort = Number(process.env.LISTEN_PORT || 80);
-        this.db = {
+    return {
+        webPath: process.env.WEB_PATH || "",
+        listenPort: Number(process.env.LISTEN_PORT || 80),
+        issuer: process.env.ISSUER || "",
+        db: {
             host: process.env.DB_HOST || "",
             port: Number(process.env.DB_PORT || 0),
             name: process.env.DB_NAME || "",
@@ -59,9 +25,8 @@ export class Config implements ConfigIF {
                 users: process.env.USERS_TABLE || "",
                 refreshTokens: process.env.REFRESH_TABLE || "",
             }
-        };
-        this.issuer = process.env.ISSUER || "",
-        this.crypto = {
+        },
+        crypto: {
             currentVersion: (process.env.CURRENT_HASH_VERSION as hashVersions) || "default",
             default: {
                 iterations: Number(process.env.HASH_ITERATIONS_default),
@@ -73,33 +38,33 @@ export class Config implements ConfigIF {
             refreshTTL: Number(process.env.REFRESH_TTL),
             refreshTimeout: Number(process.env.REFRESH_TIMEOUT),
             key: {
-                private: "",
-                public: "",
-                updated: 0
+                private: privateKey,
+                public: publicKey,
+                updated: Date.now()
             }
-        };
-        this.generateKeys();
-        this.checkConfig(this, "GLOBAL_CONFIG");
+        }
+    };
+};
+
+type ConfigData = ReturnType<typeof loadData>;
+
+abstract class ConfigBase {}
+interface ConfigBase extends ConfigData {}
+
+export class Config extends ConfigBase {
+    constructor() {
+        super();
+
+        const data = loadData();
+        this.checkConfig(data, "GLOBAL_CONFIG");
+        Object.assign(this, data);
     }
 
-    private generateKeys(): void {
-        const { publicKey, privateKey } = generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: { type: 'spki', format: 'pem' },
-            privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-        });
-
-        this.crypto.key = {
-            private: privateKey,
-            public: publicKey,
-            updated: Date.now()
-        };
-    }
-
-    public checkConfig(obj: Record<string, any>, objName: string): void {
+    private checkConfig(obj: Record<string, any>, objName: string): void {
         for (const key in obj) {
             const val = obj[key];
-            if (val === undefined || val === null || (typeof val === "number" && Number.isNaN(val)) || val === "") {
+
+            if (val === undefined || val === null || val === "" || (typeof val === "number" && Number.isNaN(val))) {
                 throw new Error(`${objName}.${key} is invalid (${val}). Config failed to load.`);
             }
             if (typeof val === "object" && !Array.isArray(val)) {
