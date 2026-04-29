6a3ab6fd0e25faebdc0f3a656e3dfefc92194bc3
RealRusLink <real.rus.link@gmail.com>
2026-04-29 16:28:38 +0300

    refactor&feat: refactored register and login endpoints(authenticate renamed to login), implemented redirection handler for third-party services

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,6 @@
 import {type Context, Hono} from "hono";
 import {setCookie, deleteCookie, getCookie} from "hono/cookie"
-import type {DBAdapter} from "../db/adapter.js";
+import type {DBAdapter, User} from "../db/adapter.js";
 import type {Config, hashVersions} from "../config.js";
 import {BusinessError, InfrastructureError} from "../errors/types.js";
 import z from "zod"
@@ -37,7 +37,7 @@ export class Api extends Hono{
      */
     setupRoutes(){
         this.post("/register", (c) => this.register(c))
-        this.post("/authenticate", (c) => this.authenticate(c))
+        this.post("/login", (c) => this.login(c))
         this.post("/rotate", (c) => this.rotation(c))
         this.post("/user/logout", (c) => this.logOut(c))
         this.post("/user/removejwt", (c) => this.removeJWT(c))
@@ -65,93 +65,102 @@ export class Api extends Hono{
     }
 
 
+    async verifyUserCredentials(credentials: object){
+        const credentialsFormSchema = z.object({
+            password: z.string().max(255).min(8),
+            username: z.string().max(255).min(3),
+            email: z.email()
+        });
+        const validationResult = credentialsFormSchema.safeParse(credentials)
+        if (!validationResult.success) throw new BusinessError("Bad credentials", 400)
+        return validationResult.data;
+    }
+
+
+
+    async handleRedirection(c: Context, savedUserData: User){
+        const redirect_url = getCookie(c, "redirect_url");
+        const auditor = getCookie(c, "auditor");
+        const contextParameters = z.object({
+            redirect_url: z.url(),
+            auditor: z.string(),
+        })
+        const contextValidationResult = contextParameters.safeParse({redirect_url, auditor});
+        deleteCookie(c, "redirect_url");
+        deleteCookie(c, "auditor");
+
+        if (contextValidationResult.success){
+            const foreignJWT = this.TokenApi.generateJWT(savedUserData.uuid, savedUserData.email, savedUserData.username, contextValidationResult.data.auditor);
+            const fullRedirect = new URL(contextValidationResult.data.redirect_url);
+            fullRedirect.searchParams.set("code", "temp_code")
+            fullRedirect.protocol = "http";
+            console.log(fullRedirect.toJSON())
+            return {overrideResponse: true, response: c.redirect(fullRedirect.toString(), 303)};
+        }
+        return {overrideResponse: false}
+    }
+
 
     /**
      * Registers user
      */
     async register(c: Context){
-        const registrationFormSchema = z.object({
-            password: z.string().max(255).min(8),
-            username: z.string().max(255).min(3),
-            email: z.email()
-        })
-        const form = await c.req.json()
 
-        const validationResult = registrationFormSchema.safeParse(form)
-        if (!validationResult.success) throw new BusinessError("Bad credentials", 400)
+        const userShouldNotExist = async (username: string, email: string) => {
+            const userDataRequest = await this.DBApi.getUser(username, email);
+            if (userDataRequest.success) throw new BusinessError("User already exists", 401)
+            if (userDataRequest.reason !== "User doesn't exist") throw new BusinessError(userDataRequest.reason, 401)
+        }
 
-        const data = validationResult.data
+        const saveUser = async (username: string, email: string, password: string) => {
+            const salt = this.PasswordApi.generateSalt();
+            const passwordHash = await this.PasswordApi.hashPassword(password, salt);
 
-        const userDataRequest = await this.DBApi.getUser(data.username, data.email);
-        if (userDataRequest.success) throw new BusinessError("User already exists", 401)
-        if (userDataRequest.reason !== "User doesn't exist") throw new BusinessError(userDataRequest.reason, 401)
-
-        const salt = this.PasswordApi.generateSalt();
-
-        const passwordHash = await this.PasswordApi.hashPassword(data.password, salt);
-
-        const registrationResult = await this.DBApi.registerUser(data.username, data.email, passwordHash, salt, this.GlobalConfig.crypto.currentVersion)
-
-        const redirect = getCookie(c, "redirect_url");
-        const auditor = getCookie(c, "auditor");
-
-        const contextParameters = z.object({
-            redirect_url: z.url(),
-            auditor: z.string(),
-        })
-
-
-        const contextValidationResult = contextParameters.safeParse({redirect_url: redirect, auditor});
-
-        if (!registrationResult.success) throw new InfrastructureError("DB error", 507)
-
-        const jwt = this.TokenApi.generateJWT(registrationResult.data.uuid, data.email, data.username);
-
-        setCookie(c, "jwt", jwt)
-        setCookie(c, "refresh", await this.#createRefresh(registrationResult.data.uuid));
-        deleteCookie(c, "redirect_url");
-        deleteCookie(c, "auditor");
-
-        if (contextValidationResult.success){
-            const foreignJWT = this.TokenApi.generateJWT(registrationResult.data.uuid, data.email, data.username, contextValidationResult.data.auditor);
-            const fullRedirect = new URL(contextValidationResult.data.redirect_url);
-            fullRedirect.searchParams.set("code", "temp_code")
-            fullRedirect.protocol = "http";
-            console.log(fullRedirect.toJSON())
-            return c.redirect(fullRedirect.toString(), 303);
+            const registrationResult = await this.DBApi.registerUser(username, email, passwordHash, salt, this.GlobalConfig.crypto.currentVersion)
+            if (!registrationResult.success) throw new InfrastructureError("DB error", 507)
+            return registrationResult.data;
         }
 
 
-        return c.text(jwt, 200);
+        const registrationData = await this.verifyUserCredentials(await c.req.json())
+
+        await userShouldNotExist(registrationData.username, registrationData.email);
+
+        const savedUserData = await saveUser(registrationData.username, registrationData.email, registrationData.password)
+
+        const jwt = this.TokenApi.generateJWT(savedUserData.uuid, savedUserData.email, savedUserData.username);
+
+        setCookie(c, "jwt", jwt)
+        setCookie(c, "refresh", await this.#createRefresh(savedUserData.uuid));
+
+
+        const redirection = await this.handleRedirection(c, savedUserData);
+
+
+        return redirection.overrideResponse ? redirection.response : c.text(jwt, 200);
     }
 
 
-    async authenticate(c: Context){
-        const loginFormSchema = z.object({
-            password: z.string().max(255).min(8),
-            username: z.string().max(255).min(3),
-            email: z.email()
-        })
+    async login(c: Context){
         const form = await c.req.json()
+        const getUser = async (username: string, email: string) => {
+            const dbRequest = await this.DBApi.getUser(username, email);
+            if (!dbRequest.success) throw new BusinessError("Invalid credentials", 400)
+            return dbRequest.data;
+        }
 
-        const validationResult = loginFormSchema.safeParse(form)
+        const inputUserData = await this.verifyUserCredentials(form);
 
-        if (!validationResult.success) throw new BusinessError("Bad credentials", 400)
+        const userData = await getUser(inputUserData.username, inputUserData.email);
 
-        const inputUserData = validationResult.data
-        const dbRequest = await this.DBApi.getUser(inputUserData.username, inputUserData.email);
-
-        if (!dbRequest.success) throw new BusinessError("Invalid credentials", 400)
-
-        const dbUserData = dbRequest.data;
-
-        const passwordCheck =  await this.PasswordApi.verifyPassword(inputUserData.password, dbUserData.salt, dbUserData.password_hash, dbUserData.hash_version as hashVersions)
-
+        const passwordCheck =  await this.PasswordApi.verifyPassword(inputUserData.password, userData.salt, userData.password_hash, userData.hash_version as hashVersions)
         if (!passwordCheck) throw new BusinessError("Invalid credentials", 400)
-        const jwt = this.TokenApi.generateJWT(dbUserData.uuid, dbUserData.email, dbUserData.username);
+
+        const jwt = this.TokenApi.generateJWT(userData.uuid, userData.email, userData.username);
         setCookie(c, "jwt", jwt);
-        setCookie(c, "refresh", await this.#createRefresh(dbUserData.uuid));
-        return c.text(jwt, 200)
+        setCookie(c, "refresh", await this.#createRefresh(userData.uuid));
+        const redirection = await this.handleRedirection(c, userData);
+        return redirection.overrideResponse ? redirection.response : c.text(jwt, 200);
     }
 
 
